5c8a1938a43ea0d7a52799c79435e2bdc3f5dd90
Xin <129676368+endlessnoc@users.noreply.github.com>
2023-08-26 00:47:46 +0800

    Basketball Game Brainteaser.py

--- Basketball Game Brainteaser.py
+++ Basketball Game Brainteaser.py
@@ -2,26 +2,29 @@
 #請問A、B這兩個隊伍打一場比賽，誰的贏面更大？
 #每個球隊進球之後，都會由對方發球。（球隊B投球會有搶籃板的問題）
 #此問題假設兩個隊伍的搶籃板能力相同，不考慮抄截罰球等問題。
+#There are two teams, A and B. 
+#Team A only shoots two-point shots and has a perfect shooting record with a 100% FG. 
+#Team B only takes three-point shots, giving them a shooting accuracy of 66.67% (2/3).
+#After each successful score shot, the ball will turn to the other team. (Only Team B's shooting will results in rebounds.)
+#Assumes that 2 teams have equal rebounding abilities and does'nt consider other factors such as stl or FT.
+
 
 import random
-
-# 球隊A得分和出手次數
+# Team A
 a_score = 0
 a_control_times = 0
 
-# 球隊B得分和出手次數
+# Team B
 b_score = 0
 b_control_times = 0
-# 模擬球隊B的命中率
 goal_list = [True, True, False]
 
-# 隨機選擇哪只球隊開球
 control_ball = random.choice(['A','B'])
 for i in range(100):
     if control_ball == 'A':
         a_control_times = a_control_times + 1
         a_score = a_score + 2
-        print('%d. 球隊[A]出手，命中，球隊A和球隊B當前比分為 %d ：%d'%(
+        print('%d. Team[A] Score.\n Scoreboard: \n Team A: TeamB =  %d ：%d'%(
               i+1, a_score, b_score)) 
         control_ball = 'B'
     else:
@@ -29,18 +32,18 @@ for i in range(100):
         goal = random.choice(goal_list)
         if goal:
             b_score = b_score + 3
-            print('%d. 球隊[B]出手，命中，球隊A和球隊B當前比分為 %d ：%d'%(
+            print('%d. Team[B] Score.\n Scoreboard: \n Team A: TeamB = %d ：%d'%(
                   i+1, a_score, b_score)) 
             control_ball = 'A'
         else:
-            print('%d. 球隊[B]出手，投失，球隊A和球隊B當前比分為 %d ：%d'%(
+            print('%d. Team[B] Miss.\n Scoreboard: \n Team A: TeamB = %d ：%d'%(
                   i+1, a_score, b_score)) 
-            # 模擬搶籃板
-            control_ball = random.choice(['A','B'])    
+            # Rebounds
+            control_ball = random.choice(['A', 'B'])    
 
-print('比賽結束！')
-print('球隊A累計出手次數：%d，最終得分：%d'%(a_control_times, a_score))
-print('球隊B累計出手次數：%d，最終得分：%d'%(b_control_times, b_score))
+print('Game End！')
+print('Team[A] Number of Shots：%d，Score：%d'%(a_control_times, a_score))
+print('Team[B] Number of Shots：%d，Score：%d'%(b_control_times, b_score))
 if a_score> b_score: 
     print("A team Win!")
 elif a_score == b_score:
