38fb4dbfa7ce261de22daf005814b4e0a1626c52
Xin <129676368+endlessnoc@users.noreply.github.com>
2023-08-29 15:17:56 +0800

    Update 2 Trading Strategies Comparison.py

--- 2 Trading Strategies Comparison.py
+++ 2 Trading Strategies Comparison.py
@@ -1,13 +1,4 @@
-#有兩個相同交易頻率的策略A和B，策略A的勝率是66.67%，盈虧比是1.5，策略B的勝率是33.33%，盈虧比是4.2
-#請問在經過相同時間長度的充分交易（例如交易10000次）後，哪個策略的預期收益更高？
-#There are two strategies, A and B, with the same trading frequency. 
-#Strategy A has a win rate of 66.67% and a profit-loss ratio of 1.5.
-#Strategy B has a win rate of 33.33% and a profit-loss ratio of 4.2.
-#After a sufficient number of trades over the same period (E.g., 10,000 trades), which strategy would yield higher expected returns?
-
-
 import random
-import matplotlib
 import matplotlib.pyplot as plt
 
 N_trade = 10000 
@@ -17,7 +8,7 @@ A_t_profit_list = []
 B_t_profit = 0
 B_t_profit_list = []
 
-x_list = list(range(n_trade))
+x_list = list(range(N_trade))
 for i in x_list:
 
 #Strategt A Win Rate = 66.66%
